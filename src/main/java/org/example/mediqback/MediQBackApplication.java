9fd32a8b55265d16a490eb23a1d86999e0afb947
mihub02 <jay06holi@gmail.com>
2026-02-27 12:41:24 +0900

    [Init] 프로젝트 초기 설정

---
+++ src/main/java/org/example/mediqback/MediQBackApplication.java
@@ -0,0 +1,13 @@
+package org.example.mediqback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MediQBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MediQBackApplication.class, args);
+    }
+
+}
